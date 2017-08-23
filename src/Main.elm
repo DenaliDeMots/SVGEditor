34d29463e41ada7a8430516fa194e92a5fc4cda1
Unknown <denali.demots@gmail.com>
2017-08-23 15:25:41 -0400

    Implemented Tool Pallet

--- src/Main.elm
+++ src/Main.elm
@@ -68,7 +68,7 @@ initialModel =
     , cursorPosition = NotTracking
     , mouseDown = False
     , currentAction = None
-    , activeTool = Tool.DrawElipse
+    , activeTool = Tool.DrawRectangle
     , graphics = []
     , previewGraphic = Maybe.Nothing
     }
@@ -122,6 +122,7 @@ update msg model =
                             | mouseDown = True
                             , cursorPosition = modelUpdates.cursorPosition
                             , currentAction = modelUpdates.currentAction
+                            , activeTool = modelUpdates.activeTool
                         }
                             ! commands
 
@@ -158,12 +159,12 @@ mouseDownEvent model clickTarget position =
         model2 =
             { model | mouseDown = True }
     in
-        case clickTarget of
+        case Debug.log "mouseDown click target" clickTarget of
             ToolPallet tool ->
                 ( { model2
                     | currentAction = None
                     , activeTool = tool
-                    , cursorPosition = Pos position
+                    , cursorPosition = NotTracking
                   }
                 , []
                 )
@@ -356,7 +357,7 @@ trackPosition cursor tagger =
 --View--
 
 
-view : Model -> Svg.Svg Msg
+view : Model -> Svg Msg
 view model =
     let
         wHeight =
@@ -372,20 +373,119 @@ view model =
             , onMouseUpIsolated (System << MouseUp Screen)
             , onMouseDownIsolated (System << MouseDown Screen)
             ]
-            (model.graphics
-                ++ Utilities.maybeToList model.previewGraphic
-                |> List.map
-                    (\graphic ->
-                        Graphic.toSvg
-                            [ onMouseUpIsolated <| System << (MouseUp <| Graphic graphic)
-                            , onMouseDownIsolated <| System << (MouseDown <| Graphic graphic)
-                            ]
-                            graphic
-                    )
-            )
+        <|
+            symbols
+                ++ (model.graphics
+                        ++ Utilities.maybeToList model.previewGraphic
+                        |> List.map
+                            (\graphic ->
+                                Graphic.toSvg
+                                    [ onMouseUpIsolated <| System << (MouseUp <| Graphic graphic)
+                                    , onMouseDownIsolated <| System << (MouseDown <| Graphic graphic)
+                                    ]
+                                    graphic
+                            )
+                        |> flip (++) [ toolPallet 50 50 80 160 model.activeTool ]
+                   )
 
 
 
+--Interface Elements--
+
+
+symbols =
+    [ --Tool Pallet Symbols
+      Svg.symbol [ SvgA.id "drawRectangle", SvgA.viewBox "0 0 100 100" ]
+        [ Svg.rect
+            [ SvgA.fill "#ddeaff"
+            , SvgA.x "1"
+            , SvgA.y "1"
+            , SvgA.width "98"
+            , SvgA.height "98"
+            ]
+            []
+        , Svg.rect [ SvgA.fill "#1e1e1e", SvgA.x "20", SvgA.y "20", SvgA.width "60", SvgA.height "60" ] []
+        ]
+    , Svg.symbol [ SvgA.id "drawElipse", SvgA.viewBox "0 0 100 100" ]
+        [ Svg.rect
+            [ SvgA.fill "#ddeaff"
+            , SvgA.x "1"
+            , SvgA.y "1"
+            , SvgA.width "98"
+            , SvgA.height "98"
+            ]
+            []
+        , Svg.ellipse [ SvgA.fill "#1e1e1e", SvgA.cx "50", SvgA.cy "50", SvgA.rx "30", SvgA.ry "30" ] []
+        ]
+    ]
+
+
+toolPallet : Int -> Int -> Int -> Int -> Tool -> Svg Msg
+toolPallet x y width height activeTool =
+    let
+        toolType tool =
+            if tool == activeTool then
+                "#ff5e5e"
+            else
+                "none"
+
+        borderSize =
+            5
+
+        numberOfButtons =
+            2
+
+        heightPerButton =
+            toFloat (height - borderSize) / numberOfButtons
+
+        yPosition elementNumber =
+            elementNumber * heightPerButton + borderSize + toFloat y
+
+        xWidth =
+            width - borderSize * 2
+    in
+        Svg.g []
+            [ Svg.rect
+                --background
+                [ SvgA.x <| toString x
+                , SvgA.y <| toString y
+                , SvgA.width <| toString width
+                , SvgA.height <| toString height
+                , SvgA.fill "#0254d8"
+                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.ToolPalletHandle))
+                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.ToolPalletHandle))
+                ]
+                []
+            , Svg.use
+                [ SvgA.xlinkHref "#drawRectangle"
+                , SvgA.x <| toString (x + borderSize)
+                , SvgA.y <| toString (yPosition 0)
+                , SvgA.width <| toString xWidth
+                , SvgA.height <| toString heightPerButton
+                , SvgA.stroke <| toolType Tool.DrawRectangle
+                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.DrawRectangle))
+                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.DrawRectangle))
+                ]
+                []
+            , Svg.use
+                [ SvgA.xlinkHref "#drawElipse"
+                , SvgA.x <| toString (x + borderSize)
+                , SvgA.y <| toString (yPosition 1)
+                , SvgA.width <| toString xWidth
+                , SvgA.height <| toString heightPerButton
+                , SvgA.stroke <| toolType Tool.DrawElipse
+                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.DrawElipse))
+                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.DrawElipse))
+                ]
+                []
+            ]
+
+
+
+--onMouseUpIsolated (System << MouseUp Screen)
+--onMouseDownIsolated (System << MouseDown Screen)
+--onMouseUpIsolated <| System << (MouseUp <| Graphic graphic)
+--onMouseDownIsolated <| System << (MouseDown <| Graphic graphic)
 --View--
 ----------------------------------------------------------------------
 -----------------------------------------------------------------------
@@ -405,16 +505,12 @@ type alias Position =
 type ClickTarget
     = Graphic Graphic.Graphic
     | Screen
-    | ToolPallet Tool.Tool
+    | ToolPallet Tool
 
 
 type Action
     = None
     | Draw DrawAction
-
-
-
---|SelectTool
 
 
 type DrawAction
