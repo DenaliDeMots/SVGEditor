e570fdf87e6fa7499bc09c8388d70f0336f7f775
Unknown <denali.demots@gmail.com>
2017-08-24 15:47:58 -0400

    Implemented Tool selection box

--- src/Main.elm
+++ src/Main.elm
@@ -395,27 +395,46 @@ view model =
 
 symbols =
     [ --Tool Pallet Symbols
-      Svg.symbol [ SvgA.id "drawRectangle", SvgA.viewBox "0 0 100 100" ]
-        [ Svg.rect
-            [ SvgA.fill "#ddeaff"
-            , SvgA.x "1"
-            , SvgA.y "1"
-            , SvgA.width "98"
-            , SvgA.height "98"
-            ]
-            []
-        , Svg.rect [ SvgA.fill "#1e1e1e", SvgA.x "20", SvgA.y "20", SvgA.width "60", SvgA.height "60" ] []
+      Svg.symbol [ SvgA.id "drawRectangle", SvgA.viewBox "0 0 100 100", SvgA.preserveAspectRatio "none" ]
+        [ Svg.use [ SvgA.xlinkHref "#buttonBackground" ] []
+        , Svg.use [ SvgA.xlinkHref "#drawRectangleIcon" ] []
         ]
-    , Svg.symbol [ SvgA.id "drawElipse", SvgA.viewBox "0 0 100 100" ]
+    , Svg.symbol [ SvgA.id "drawElipse", SvgA.viewBox "0 0 100 100", SvgA.preserveAspectRatio "none" ]
+        [ Svg.use [ SvgA.xlinkHref "#buttonBackground" ] []
+        , Svg.use [ SvgA.xlinkHref "#drawElipseIcon" ] []
+        ]
+    , Svg.symbol [ SvgA.id "buttonBackground", SvgA.viewBox "0 0 100 100", SvgA.preserveAspectRatio "none" ]
         [ Svg.rect
             [ SvgA.fill "#ddeaff"
-            , SvgA.x "1"
-            , SvgA.y "1"
-            , SvgA.width "98"
-            , SvgA.height "98"
+            , SvgA.stroke "none"
+            , SvgA.x "0"
+            , SvgA.y "0"
+            , SvgA.width "100"
+            , SvgA.height "100"
             ]
             []
-        , Svg.ellipse [ SvgA.fill "#1e1e1e", SvgA.cx "50", SvgA.cy "50", SvgA.rx "30", SvgA.ry "30" ] []
+        , Svg.symbol [ SvgA.id "drawRectangleIcon", SvgA.viewBox "0 0 100 100", SvgA.preserveAspectRatio "xMidYMid" ]
+            [ Svg.rect
+                [ SvgA.fill "#1e1e1e"
+                , SvgA.stroke "none"
+                , SvgA.x "20"
+                , SvgA.y "20"
+                , SvgA.width "60"
+                , SvgA.height "60"
+                ]
+                []
+            ]
+        ]
+    , Svg.symbol [ SvgA.id "drawElipseIcon", SvgA.viewBox "0 0 100 100", SvgA.preserveAspectRatio "xMidYMid" ]
+        [ Svg.ellipse
+            [ SvgA.fill "#1e1e1e"
+            , SvgA.stroke "none"
+            , SvgA.cx "50"
+            , SvgA.cy "50"
+            , SvgA.rx "30"
+            , SvgA.ry "30"
+            ]
+            []
         ]
     ]
 
@@ -423,12 +442,6 @@ symbols =
 toolPallet : Int -> Int -> Int -> Int -> Tool -> Svg Msg
 toolPallet x y width height activeTool =
     let
-        toolType tool =
-            if tool == activeTool then
-                "#ff5e5e"
-            else
-                "none"
-
         borderSize =
             5
 
@@ -436,15 +449,128 @@ toolPallet x y width height activeTool =
             2
 
         heightPerButton =
-            toFloat (height - borderSize) / numberOfButtons
+            toFloat (height - borderSize) / numberOfButtons - borderSize
 
         yPosition elementNumber =
-            elementNumber * heightPerButton + borderSize + toFloat y
+            elementNumber * (heightPerButton + borderSize) + toFloat y + borderSize
 
         xWidth =
             width - borderSize * 2
+
+        selectionBoxThickness =
+            7
+
+        selectionColor =
+            "#ff5e5e"
+
+        buttonList =
+            --Determines which buttons appear and in what order
+            [ Tool.DrawRectangle, Tool.DrawElipse ]
+
+        getToolFunction tool =
+            case tool of
+                Tool.Select ->
+                    Debug.crash "TODO implement select button"
+
+                Tool.DrawRectangle ->
+                    rectangleButton
+
+                Tool.DrawElipse ->
+                    elipseButton
+
+                Tool.ToolPalletHandle ->
+                    Debug.crash "TODO implement draggable tool pallet"
+
+        positionedButtons =
+            positionButtons buttonList 0
+
+        positionButtons buttonList index =
+            case buttonList of
+                [] ->
+                    []
+
+                tool :: tools ->
+                    let
+                        f =
+                            getToolFunction tool
+                    in
+                        if tool == activeTool then
+                            f index
+                                :: selectionBox index
+                                :: (positionButtons tools <| index + 1)
+                        else
+                            f index :: (positionButtons tools <| index + 1)
+
+        rectangleButton index =
+            Svg.use
+                [ SvgA.xlinkHref "#drawRectangle"
+                , SvgA.x <| toString (x + borderSize)
+                , SvgA.y <| toString (yPosition index)
+                , SvgA.width <| toString xWidth
+                , SvgA.height <| toString heightPerButton
+                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.DrawRectangle))
+                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.DrawRectangle))
+                ]
+                []
+
+        elipseButton index =
+            Svg.use
+                [ SvgA.xlinkHref "#drawElipse"
+                , SvgA.x <| toString (x + borderSize)
+                , SvgA.y <| toString (yPosition index)
+                , SvgA.width <| toString xWidth
+                , SvgA.height <| toString heightPerButton
+                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.DrawElipse))
+                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.DrawElipse))
+                ]
+                []
+
+        selectionBox index =
+            Svg.g []
+                --Selection icon to draw on top of selected tool button
+                [ Svg.rect
+                    --Top Line
+                    [ SvgA.x <| toString (x + borderSize)
+                    , SvgA.y <| toString (yPosition index)
+                    , SvgA.width <| toString xWidth
+                    , SvgA.height <| toString selectionBoxThickness
+                    , SvgA.fill selectionColor
+                    , SvgA.stroke "none"
+                    ]
+                    []
+                , Svg.rect
+                    --Bottom Line
+                    [ SvgA.x <| toString (x + borderSize)
+                    , SvgA.y <| toString (yPosition index + heightPerButton - selectionBoxThickness)
+                    , SvgA.width <| toString xWidth
+                    , SvgA.height <| toString selectionBoxThickness
+                    , SvgA.fill selectionColor
+                    , SvgA.stroke "none"
+                    ]
+                    []
+                , Svg.rect
+                    --Left Line
+                    [ SvgA.x <| toString (x + borderSize)
+                    , SvgA.y <| toString (yPosition index)
+                    , SvgA.width <| toString selectionBoxThickness
+                    , SvgA.height <| toString heightPerButton
+                    , SvgA.fill selectionColor
+                    , SvgA.stroke "none"
+                    ]
+                    []
+                , Svg.rect
+                    --Right Line
+                    [ SvgA.x <| toString (x + borderSize + xWidth - selectionBoxThickness)
+                    , SvgA.y <| toString (yPosition index)
+                    , SvgA.width <| toString selectionBoxThickness
+                    , SvgA.height <| toString heightPerButton
+                    , SvgA.fill selectionColor
+                    , SvgA.stroke "none"
+                    ]
+                    []
+                ]
     in
-        Svg.g []
+        Svg.g [] <|
             [ Svg.rect
                 --background
                 [ SvgA.x <| toString x
@@ -456,29 +582,8 @@ toolPallet x y width height activeTool =
                 , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.ToolPalletHandle))
                 ]
                 []
-            , Svg.use
-                [ SvgA.xlinkHref "#drawRectangle"
-                , SvgA.x <| toString (x + borderSize)
-                , SvgA.y <| toString (yPosition 0)
-                , SvgA.width <| toString xWidth
-                , SvgA.height <| toString heightPerButton
-                , SvgA.stroke <| toolType Tool.DrawRectangle
-                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.DrawRectangle))
-                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.DrawRectangle))
-                ]
-                []
-            , Svg.use
-                [ SvgA.xlinkHref "#drawElipse"
-                , SvgA.x <| toString (x + borderSize)
-                , SvgA.y <| toString (yPosition 1)
-                , SvgA.width <| toString xWidth
-                , SvgA.height <| toString heightPerButton
-                , SvgA.stroke <| toolType Tool.DrawElipse
-                , onMouseUpIsolated (System << MouseUp (ToolPallet Tool.DrawElipse))
-                , onMouseDownIsolated (System << MouseDown (ToolPallet Tool.DrawElipse))
-                ]
-                []
             ]
+                ++ positionedButtons
 
 
 
